3a9b5ce60fdf70d14a9053dcca129088ed008510
master <joelpalacio630@gmail.com>
2024-02-22 18:11:48 -0600

    Feat: Introducción a Testing con Jest

---
+++ Testing-Jest/__tests__/holamundo.js
@@ -0,0 +1,15 @@
+/*
+Es importante agrupar las pruebas en un grupo por medio de describe(),
+esto permite agrupar por tipos similares.
+* cada prueba se invoca por medio de it() o de test(), ya que ambas funciones hacen lo mismo.
+*/
+
+
+describe( 'Grupo de pruebas', () => {
+
+    it( 'Hola mundo en Jest usando la palabra reservada IT', () => { } );
+
+    test( 'Hola mundo en Jest usando la palabra reservada TEST', () => { } );
+
+} )
+
